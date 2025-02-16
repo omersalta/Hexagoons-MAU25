4f60a95dd62e62989ab0b079939e20c27ac85a64
Muhammed Ali CAN <tmwwdw@gmail.com>
2025-02-16 14:43:57 +0300

    feat: add main camera min

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,13 +3,14 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public float drag = 0.08F;
+    public float min = 0;
     public float max = 1000;
     public Rigidbody2D target;
 
     void FixedUpdate()
     {
         Vector3 pos = Vector3.Lerp(transform.position, new Vector3(target.position.x, 0, -1), drag);
-        pos.x = Mathf.Clamp(pos.x, -4, max);
+        pos.x = Mathf.Clamp(pos.x, min, max);
         transform.position = pos;
     }
 }
