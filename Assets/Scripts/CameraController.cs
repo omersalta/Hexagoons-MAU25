1d17a12fd533df05f9a58377c06077470bde4cdb
Muhammed Ali CAN <tmwwdw@gmail.com>
2025-02-16 08:29:25 +0300

    feat: add simple tutorial texts

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,12 +3,13 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public float drag = 0.08F;
+    public float max = 1000;
     public Rigidbody2D target;
 
     void FixedUpdate()
     {
         Vector3 pos = Vector3.Lerp(transform.position, new Vector3(target.position.x, 0, -1), drag);
-        pos.x = Mathf.Clamp(pos.x, 0, 1000);
+        pos.x = Mathf.Clamp(pos.x, -4, max);
         transform.position = pos;
     }
 }
