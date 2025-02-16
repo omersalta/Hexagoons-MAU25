ee37204e0f5dc619ee695040a41d146f11b5c6a9
Muhammed Ali CAN <tmwwdw@gmail.com>
2025-02-16 15:30:06 +0300

    feat: more level 2

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,19 +1,20 @@
 using Game;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class PlayerMovement : MonoBehaviour,ILightDarkBehaviour
+public class PlayerMovement : MonoBehaviour, ILightDarkBehaviour
 {
     private int JumpCountOnGrounded = 1;
     public Rigidbody2D rb;
     public Animator animator;
-    [SerializeField]private SpriteRenderer _renderer;
-    
+    [SerializeField] private SpriteRenderer _renderer;
+
     private float _moveSpeed = 5f;
     private float _acceleration = 5f;
     private float _deceleration = 5f;
     private float _jumpForce = 14f;
     private int _currentJumpCount = 1;
-   
+
     private Vector2 _currentVelocity;
     private bool _isGrounded;
 
@@ -23,24 +24,25 @@ public class PlayerMovement : MonoBehaviour,ILightDarkBehaviour
         {
             return;
         }
-        
+
         // Kullanıcı girişlerini al
         float moveInput = Input.GetAxisRaw("Horizontal");
         Vector2 targetVelocity = new Vector2(moveInput * _moveSpeed, rb.velocity.y);
-        
+
         // Hızlanma ve yavaşlama hesaplamaları
         _currentVelocity = Vector2.Lerp(rb.velocity, targetVelocity, (Mathf.Abs(moveInput) > 0 ? _acceleration : _deceleration) * Time.deltaTime);
-        
+
         // Rigidbody'ye yeni hızı uygula
         rb.velocity = new Vector2(_currentVelocity.x, rb.velocity.y);
 
-        
-        if (rb.velocity.x > 0.1f )
+
+        if (rb.velocity.x > 0.1f)
         {
             animator.SetBool("Running", true);
             _renderer.flipX = false;
-            
-        }else if (rb.velocity.x < -0.1f)
+
+        }
+        else if (rb.velocity.x < -0.1f)
         {
             animator.SetBool("Running", true);
             _renderer.flipX = true;
@@ -49,7 +51,7 @@ public class PlayerMovement : MonoBehaviour,ILightDarkBehaviour
         {
             animator.SetBool("Running", false);
         }
-        
+
         // Zıplama
         if (Input.GetButtonDown("Jump"))
         {
@@ -58,22 +60,27 @@ public class PlayerMovement : MonoBehaviour,ILightDarkBehaviour
                 rb.velocity = new Vector2(rb.velocity.x, _jumpForce);
                 _currentJumpCount--;
             }
-            
+
         }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
-     {
+    {
         if (collision.gameObject.CompareTag("Ground"))
         {
             _currentJumpCount = JumpCountOnGrounded;
+        }
+        else if (collision.gameObject.CompareTag("Void"))
+        {
+            string currentSceneName = SceneManager.GetActiveScene().name;
+            SceneManager.LoadScene(currentSceneName);
         }
         else
         {
             Debug.Log("collision not grounded : " + collision.gameObject.name);
         }
     }
-    
+
     public void OnLight()
     {
         _moveSpeed = 5f;
@@ -82,7 +89,7 @@ public class PlayerMovement : MonoBehaviour,ILightDarkBehaviour
         _jumpForce = 14f;
         JumpCountOnGrounded = 2;
     }
-    
+
     public void OnDark()
     {
         _moveSpeed = 4f;
